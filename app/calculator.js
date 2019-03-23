1638a714b92bedabaedd85070c3b457a083e8651
zoe kelman <zoey247@aol.com>
2019-03-23 08:06:44 +0000

    Day2

--- app/calculator.js
+++ app/calculator.js
@@ -1,7 +1,49 @@
 function add(a, b) {
     return a + b;
 }
+function subtract (a, b){
+    return a - b;
+}
+function multiply (a, b){
+    return a * b;
+}
+
+function isOdd (num){
+    if (num % 2 == 1) {
+        return true
+    }
+    if (num % 2 == 0){
+        return false
+    }
+}
+function isEven (num){
+    if (num % 2 == 0) {
+        return true
+    }
+    if (num % 2 == 1){
+        return false
+    }
+}
+
+function divisibleBy (a, b) {
+    if (a % b == 0) 
+        return true
+        else
+        return false
+    
+}
+function square (a, b){
+    return a*a == b;
+}
 
 module.exports = {
-    add
+    add, 
+    subtract,
+    multiply,
+    isOdd,
+    isEven,
+    divisibleBy,
+    square
+
+
 };
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -3,3 +3,36 @@ const calculator = require('../app/calculator');
 test('adds 1 + 2 to equal 3', () => {
   expect(calculator.add(1, 2)).toBe(3);
 });
+
+test('subtract 2 - 1 to equal 1', () => {
+  expect(calculator.subtract(2, 1)).toBe(1);
+});
+
+
+test('multiply 1 * 1 to equal 1', () => {
+  expect(calculator.multiply(1, 1)).toBe(1);
+});
+
+test('find if 3 is odd to equal true', () => {
+  expect(calculator.isOdd(3)).toBe(true);
+});
+test('find if 2 is odd to equal false', () => {
+  expect(calculator.isOdd(2)).toBe(false);
+});
+
+test('find if 3 is even to equal false', () => {
+  expect(calculator.isEven(3)).toBe(false);
+});
+test('find if 2 is even to equal true', () => {
+  expect(calculator.isEven(2)).toBe(true);
+});
+
+test('divisiblyBy 10 / 2 to equal true', () => {
+  expect(calculator.divisibleBy(10, 2)).toBe(true);
+});
+
+test ('Squared number', () => {
+  expect (calculator.square(2)).toBe(4)
+}
+)
+
